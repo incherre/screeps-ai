d70a499c9bee916a1800fca5ce55b67e3def8e8c
Daniel Fitzick <incherre2526@gmail.com>
2018-11-01 13:15:15 -0400

    Added logging for Grafana

--- main.js
+++ main.js
@@ -32,6 +32,35 @@ var _getLabWith = function(room, resource) {
     return backup;
 }
 
+var logStuff = function() {
+    if(!Memory.stats){ Memory.stats = {}; }
+    Memory.stats['cpu.getUsed'] = Game.cpu.getUsed();
+    Memory.stats['cpu.limit'] = Game.cpu.limit;
+    Memory.stats['cpu.bucket'] = Game.cpu.bucket;
+    
+    Memory.stats['gcl.progress'] = Game.gcl.progress;
+    Memory.stats['gcl.progressTotal'] = Game.gcl.progressTotal;
+    Memory.stats['gcl.level'] = Game.gcl.level;
+    
+    for(let roomName in Game.rooms) {
+        let room = Game.rooms[roomName];
+        if(room.controller && room.controller.my) {
+            Memory.stats['rooms.' + roomName + '.rcl.level'] = room.controller.level;
+            Memory.stats['rooms.' + roomName + '.rcl.progress'] = room.controller.progress;
+            Memory.stats['rooms.' + roomName + '.rcl.progressTotal'] = room.controller.progressTotal;
+    
+            Memory.stats['rooms.' + roomName + '.spawn.energy'] = room.energyAvailable;
+            Memory.stats['rooms.' + roomName + '.spawn.energyTotal'] = room.energyCapacityAvailable;
+    
+            if(room.storage){
+                Memory.stats['rooms.' + roomName + '.storage.energy'] = room.storage.store.energy;
+            }
+            
+            Memory.stats['rooms.' + roomName + '.enemies'] = find.getHostileCreeps(room).length;
+        }
+    }
+}
+
 module.exports.loop = function () {
     creepControl.controlCreeps();
     roomControl.controlEstablishedRooms();
